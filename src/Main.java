033a8013174f9082582065aae4768c287135171f
Josu-Ferreyra <josueferreyra8824@gmail.com>
2025-10-19 21:43:59 -0300

    Modificar Main para que acceda al Login cuando se inicia la APP

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import models.dao.UsuarioDAO;
+import views.VistaLogin;
+
+import javax.swing.*;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("HELLO WORLD!");
-  }
+    public static void main(String[] args) {
+        // Para asegurar que la UI se ejecute en el hilo de despacho de eventos de Swing
+        SwingUtilities.invokeLater(() -> {
+            VistaLogin vista = new VistaLogin();
+            UsuarioDAO modelo = new UsuarioDAO();
+            ControladorLogin controlador = new ControladorLogin(vista, modelo);
+            controlador.iniciar();
+        });
+    }
 }
